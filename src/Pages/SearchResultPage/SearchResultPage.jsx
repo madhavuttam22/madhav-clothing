9d502eb38b315d01b5ae790f57e4b3e3202d3531
Sandeep6268 <sandeepsinghkhicchi@example.com>
2025-07-06 22:48:31 +0530

    commit

--- src/Pages/SearchResultPage/SearchResultPage.jsx
+++ src/Pages/SearchResultPage/SearchResultPage.jsx
@@ -6,7 +6,7 @@ import Header from "../../component/Header/Header";
 import Footer from "../../component/Footer/Footer";
 import Filters from "../../component/Filters/Filters";
 import BackToTop from "../../component/BackToTop/BackToTop";
-import "./SearchResultsPage.css";
+import "./SearchResultPage.css";
 
 const SearchResults = () => {
   const [searchParams] = useSearchParams();
