07d3ee470e902f460d7a035f47f89d7d8d3383f9
Sandeep6268 <sandeepsinghkhicchi@example.com>
2025-07-15 22:45:53 +0530

    header updatation

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,54 +1,19 @@
-// // import { Outlet, useLocation } from 'react-router-dom';
-// // import Header from '../Header/Header';
-// // import Footer from '../Footer/Footer';
-// // import { useEffect } from 'react';
-
-// // const Layout = () => {
-// //   const location = useLocation();
-
-// //   useEffect(() => {
-// //     window.scrollTo(0, 0);
-// //   }, [location.pathname]);
-
-// //   return (
-// //     <>
-// //       <Header />
-// //       <main>
-// //         <Outlet />
-// //       </main>
-// //       <Footer />
-// //     </>
-// //   );
-// // };
-
-// // export default Layout;
-
-// const Layout = () => {
-//   const location = useLocation();
-
-//   return (
-//     <>
-//       <Header />
-//       {/* 👇 Move the key directly to Outlet level */}
-//       <Outlet key={location.pathname} />
-//       <Footer />
-//     </>
-//   );
-// };
-
-
-// Layout.jsx
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { useEffect } from 'react';
 
 const Layout = () => {
   const location = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <>
       <Header />
-      <main key={location.pathname}>
+      <main>
         <Outlet />
       </main>
       <Footer />
@@ -57,3 +22,4 @@ const Layout = () => {
 };
 
 export default Layout;
+
